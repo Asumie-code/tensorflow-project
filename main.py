9a137f3971214210134de9ee2b5106dad01264fa
Asumie-code <darkeangel.1998.ia@gmail.com>
2022-11-11 20:23:54 +0100

    learn: add  recognition model

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-import tensorflow as tf 
-import numpy as np 
-from tensorflow import keras 
+import matplotlib.pyplot as plt # awesome library for vizualising data, good thing i discover this
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+mnist = tf.keras.datasets.fashion_mnist
 
 
-model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+
+#normalize the data 
+training_images = training_images / 255.0
+test_images = test_images / 255.0
 
 
-model.compile(optimizer='sgd', loss='mean_squared_error')
+# plt.imshow(training_images[0])
+# print(training_labels[0])
+# print(training_images[0])
 
-xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
-ys = np.array([-2.0, 1.0, 4.0, 7.0, 10.0, 13.0], dtype=float)
 
-model.fit(xs, ys, epochs=100)
-# y = 3x + 1
-print(model.predict([10.0]))
+
+
+
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+    
+])
+
+
+model.compile(
+    optimizer = tf.keras.optimizers.Adam(),
+    loss = 'sparse_categorical_crossentropy',
+    metrics = ['accuracy']
+)
+
+model.fit(training_images, training_labels, epochs = 5)
+
+model.evaluate(test_images, test_labels)
+
